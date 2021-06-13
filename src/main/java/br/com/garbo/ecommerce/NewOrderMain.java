6236aade5051ee5ff3a5ce9c730dcbb1b7a87725
Fernando Garbo <nando.garbo@gmail.com>
2021-06-13 18:11:26 -0300

    initial files

---
+++ src/main/java/br/com/garbo/ecommerce/NewOrderMain.java
@@ -0,0 +1,11 @@
+package br.com.garbo.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class NewOrderMain {
+
+}
